0af44d6b4fcdcfd9160d32c60f2a645d8c8d91f1
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-24 11:25:04 +0300

    Array_Lessons is work

--- ArrayLessons/Program.cs
+++ ArrayLessons/Program.cs
@@ -12,35 +12,61 @@ namespace ArrayLessons
         static void Main(string[] args)
         {
             int[] partAircraft = { 10, 12, 50, 50, 30 };
-
-            Console.SetCursorPosition(0, 20);
-            for (int i = 0; i < partAircraft.Length; i++)
+            bool onProgramm = true;
+            while (onProgramm)
             {
-                Console.ForegroundColor = ConsoleColor.DarkBlue;
-                Console.WriteLine($"Количество мест в отсеке {i + 1}  - {partAircraft[i]}");
-            }
+                Console.SetCursorPosition(0, 20);
+                for (int i = 0; i < partAircraft.Length; i++)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.WriteLine($"Количество мест в отсеке {i + 1}  - {partAircraft[i]}");
+                }
 
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.SetCursorPosition(0, 0);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(0, 0);
 
-            Console.Write($"Выберите действие:\n1 - забронировать билеты,\n2 - выход из программы.\n\n\nВведите номер действия: ");
-            int choiceUser = int.Parse(Console.ReadLine());
-            switch (choiceUser)
-            {
-                case 1:
-                    Console.Write($"Выберите номер отсека: ");
-                    int numberPart = int.Parse(Console.ReadLine());
-                    Console.Write($"Введите колчество билетов: ");
-                    int numberTickets = int.Parse(Console.ReadLine());
+                Console.Write($"Выберите действие:\n1 - забронировать билеты,\n2 - выход из программы.\n\n\nВведите номер действия: ");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        Console.Write($"Выберите номер отсека: ");
+                        int numberPart = Convert.ToInt32(Console.ReadLine());
+                        Console.Write($"Введите колчество билетов: ");
+                        int numberTickets = Convert.ToInt32(Console.ReadLine());
+                        if (numberTickets > partAircraft[numberPart - 1])
+                        {
+                            Console.WriteLine($"Ошибка! Количество запрошенных билетов превышает имеющиеся.");
+                        }
+                        else
+                        {
+                            partAircraft[numberPart - 1] -= numberTickets;
+                            Console.SetCursorPosition(0, 10);
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"Операция выполнена успешно. Отсек номер - {numberPart}. Забронированно билетов  - {numberTickets}.");
+                        }
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.SetCursorPosition(0, 10);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"До свиданния.\nСпасибо что воспользовались услугами нашего сервиса.\nПриходите еще.");
+                        onProgramm = false;
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.SetCursorPosition(0, 10);
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Ошибка! Неверный номер действия");
+                        break;
 
 
-                    break;
-                case 2:
-                    Console.Clear();
-                    Console.SetCursorPosition(0, 10);
-                    Console.ForegroundColor= ConsoleColor.Green;
-                    Console.WriteLine($"До свиданния.\nСпасибо что воспользовались услугами нашего сервиса.\nПриходите еще.");
-                    break;
+                        
+
+
+                    
+                }
+                Console.ReadKey();   
+                Console.Clear();
             }
         }
     }
